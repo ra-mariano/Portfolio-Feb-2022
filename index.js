1696c8397e42b2852f0bbf5be9ef417a31a9ec2c
ra-mariano <ronan.a.mariano@gmail.com>
2022-02-25 03:32:16 -0500

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ document.getElementById("works").addEventListener("mouseover", worksOver);
 document.getElementById("works").addEventListener("mouseout", worksOut);
 document.getElementById("workspopup").addEventListener("mouseover", worksPopUp);
 document.getElementById("workspopup").addEventListener("mouseout", worksPopOut);
+document.getElementById("aboutme").addEventListener("click", aboutMe)
 
 function worksOver() {
   document.getElementById("works").style.color = "red";
-  document.getElementById("workspopup").style.visibility = "visible"
+  document.getElementById("workspopup").style.visibility = "visible";
+  document.getElementById("workspopup").animate([
+    {opacity : 0},
+  {opacity : 1}], {
+  duration: 700,
+})
+document.getElementById("bio").style.visibility = "hidden";
 }
 
 function worksOut() {
@@ -15,9 +22,29 @@ function worksOut() {
 
 function worksPopUp() {
   document.getElementById("workspopup").style.visibility = "visible"
+  
 }
 
 function worksPopOut() {
-  document.getElementById("workspopup").style.visibility = "hidden"
+  
+document.getElementById("workspopup").style.visibility = "hidden"
 }
+
+function aboutMe() {
+  document.getElementById("bio").style.visibility = "visible";
+  document.getElementById("bio").animate([
+    {opacity : 0},
+  {opacity : 1}], {
+  duration: 1500,
+})
+document.getElementById("bio").animate([
+  { transform: 'translateY(200px)' },
+  { transform: 'translateY(0px)' }], {
+duration: 1500,
+})
+
+}
+
+
+
 
